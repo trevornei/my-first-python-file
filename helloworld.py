f44b2d3177dc5461f4aabeff64e3b49625b7950b
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-01 11:54:54 -0600

    Created input function, refactored, followed by a print statement.

--- helloworld.py
+++ helloworld.py
@@ -4,5 +4,18 @@ print("Hello, world!!!")
 """
 Here is a multiline comment.
     - My first typo in python was when printing "Hello World!!!" I ran into a syntax error because I used single 'quotes'.
-    
+"""
+
+# # Creating my first function.
+# print("What's your name? ")
+# # Input takes an argument itself. 
+# input()
+
+# I have commented the above code out because input takes an argument and creates a new line.
+# Refactored code below
+input("What is your name? ")
+print("Hello, Trevor!")
+
+"""
+    Adding Return Values: 
 """

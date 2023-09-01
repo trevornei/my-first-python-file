e5a32d6ead75c227b0b8dde073e001d2dd7d19de
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-01 11:46:31 -0600

    Initial commit.

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+print("Hello, world!!!")
+
+# My first single line comment.
+"""
+Here is a multiline comment.
+    - My first typo in python was when printing "Hello World!!!" I ran into a syntax error because I used single 'quotes'.
+    
+"""

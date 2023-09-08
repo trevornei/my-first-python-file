df7113ca18aef02b671b3026e8c630106bbb1fdb
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-08 12:26:25 -0600

    New folder for JS algorithm learning environment.

---
+++ algorithm_design_js/foobar.js
@@ -0,0 +1,6 @@
+/*
+The foo bar problem is a coding challenge that is often used as an interview question for software engineers. The problem is to write a function that takes an integer as input and returns the string "Foo" if the integer is divisible by 3, "Bar" if the integer is divisible by 5, or the integer itself if it is not divisible by 3 or 5.
+*/
+
+// 1. Takes an integer as an input.
+// 2. if (int) / 3 return
